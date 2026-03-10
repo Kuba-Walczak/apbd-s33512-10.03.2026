482526616fc2491630b631a3267206a5e1786f0d
Kuba Walczak <s33512@pjwstk.edu.pl>
2026-03-10 19:48:14 +0100

    chore: added initial files and gitignore

---
+++ task1/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace task1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
