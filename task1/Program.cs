76f78e44e49671db1b01b8a069740f4d53127a01
Kuba Walczak <s33512@pjwstk.edu.pl>
2026-03-10 20:05:05 +0100

    feat: added calculateaverage method

--- task1/Program.cs
+++ task1/Program.cs
@@ -11,5 +11,19 @@ namespace task1
             string[] weatherArr = ["rainy", "cloudy", "sunny"];
             Console.WriteLine("It's currently " + weatherArr[new Random().Next(0, weatherArr.Length)]);
         }
+
+        public float CalculateAverage(int[] values)
+        {
+            if (values.Length == 0)
+            {
+                return 0;
+            }
+            float sum = 0;
+            foreach (int val in values)
+            {
+                sum += val;
+            }
+            return sum / values.Length;
+        }
     }
 }
